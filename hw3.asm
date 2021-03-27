15fc3239a96914b28411b12022a22cd7628ebad0
Kevin Li <kevinli4321@gmail.com>
2021-03-27 17:17:33 -0400

    Fill out all but the middle part of game_board, also a testing mechanism

--- hw3.asm
+++ hw3.asm
@@ -10,14 +10,15 @@
 
 # int, int load_game(GameState* state, string filename)
 load_game:
-	addi $sp, $sp, -20	# The input buffer occupies 16($sp)
-	sw $ra, 0($sp)
+	addi $sp, $sp, -24	# The input buffer occupies 16($sp)	
+	sw $ra, 0($sp)		# ?
 	sw $s0, 4($sp)	# Stores state
 	sw $s1, 8($sp)	# Stores file descriptor
 	sw $s2, 12($sp)	# Stores address of input buffer
+	sw $s3, 16($sp)	# Stores $v1 return value from the second call to readLine, useful
 	# ===========================================================
 	move $s0, $a0		# Store state so it isn't overwritten
-	addi $s2, $sp, 16	# Store address of input buffer which is at 16($sp)
+	addi $s2, $sp, 20	# Store address of input buffer which is at 20($sp)
 	
 	# Open board file
 	li $v0, 13	
@@ -35,11 +36,52 @@ load_game:
 	j end
 
 	read:
-	# Read # of top mancala stones
+	# Read # of top mancala stones (first line) + put it into board
 	move $a0, $s1		# Move file descriptor to $a0
 	move $a1, $s2		# Move input buffer to $a1
-	jal getMultiDigitValue
-	sb $v0, 0($s0)		# Store first line's value into byte 0 of state
+	jal readLine
+	sb $v0, 1($s0)		# Store first line's value into byte 1 of state (top_mancala)
+	
+	move $t0, $v1		# Copy 2-digit ASCII into $t0
+	srl $t0, $t0, 8		# Move tens digit into ones digit
+	sb $t0, 6($s0)		# Store tens digit of top_mancula into byte 6 of state
+	andi $v1, $v1, 0x00FF	# Only care about the last 8 bits of $v1 (ones digit)
+	sb $v1, 7($s0)		# Store ones digit of top_mancula into byte 7 of state
+	
+	# Read # of bottom mancala stones (second line) [can't put it into board yet]
+	move $a0, $s1		
+	move $a1, $s2		
+	jal readLine
+	sb $v0, 0($s0)		# Store second line's value into byte 0 of state (bot_mancala)
+	move $s3, $v1		# Save $v1 for when I know the amount of pockets there are
+	# Read # of pockets per row (third line)
+	move $a0, $s1		
+	move $a1, $s2		
+	jal readLine
+	sb $v0, 2($s0)		# Store third line's value into byte 2 of state (bot_pockets)
+	sb $v0, 3($s0)		# Store third line's value into byte 3 of state (top_pockets)
+		# Subtask: fill last 2 bytes of game_board
+		sll $v0, $v0, 2		# Multiply pockets by 4 so I can reach the last 2 bytes of game_board
+		addi $v0, $v0, 8	# Add the first 8 bytes
+		add $s0, $s0, $v0	# Advance state to the second to last digit
+		move $t0, $s3		# Copy 2-digit ASCII into $t0
+		srl $t0, $t0, 8		# Move tens digit into ones digit
+		sb $t0, 0($s0)		# Store tens digit of bot_mancula into 2nd to last byte of state
+		andi $s3, $s3, 0x00FF	# Only care about the last 8 bits of $s3 (ones digit)
+		sb $s3, 1($s0)		# Store ones digit of bot_mancula into last byte of state
+		sub $s0, $s0, $v0	# Move state back to original position
+		
+	# moves_executed is 0 at the beginning
+	sb $0, 4($s0)
+	
+	# player_turn is 'B' at the beginning
+	li $t0, 'B'		# Player 1
+	sb $t0, 5($s0)		# Store 'B' into byte 5 of state (player_turn)
+
+	# Loop to read contents of top row (fourth line)
+	
+	
+	# Loop to read contents of bottom row (fifth line)
 
 	# Close board file
 	li $v0, 16	
@@ -51,11 +93,12 @@ load_game:
 	lw $s0, 4($sp)
 	lw $s1, 8($sp)
 	lw $s2, 12($sp)
-	addi $sp, $sp, 20
+	lw $s3, 16($sp)
+	addi $sp, $sp, 24
 	jr $ra
 	
-# ($a0: int fd, $a1: int buffer) -> Returns multi (or single)-digit value from file
-getMultiDigitValue:
+# ($a0: int fd, $a1: int buffer) -> Returns multi (or single)-digit value from file, and 2-digit ASCII value
+readLine:
 	# 20-31 is 12 bytes to store a 12-char long string. Largest int is only 10 digits.
 	# Use 12 instead of 10 to align with a word boundary.
 	addi $sp, $sp, -24	
@@ -81,15 +124,33 @@ getMultiDigitValue:
 			li $v0, 14
 			syscall
 		foundSlashN:			# Skip character (\n)
-			j findValue
+			j find2DigitASCII
 		continue_length_loop:	
 			addi $s0, $s0, 1	# Increment length
 			sb $t2, 0($s1)		# Store character in string
 			addi $s1, $s1, 1	# Increment string pointer
 			j findLengthLoop
 	
-	findValue:
+	find2DigitASCII:
 	sb $0, 0($s1)		# Null terminate string, just feel like doing it
+	move $t1, $s1		# Copy string into $t1
+	# Subtask: put "00" - "99" into $v1
+	li $v1, '0'		# Assume tens digit is 0 if input is only 1 digit
+	sll $v1, $v1, 8		# Ones digit is 8 bits large, shift '0' to tens digit
+	addi $t1, $t1, -1
+	lbu $t0, 0($t1)		# Ones digit
+	or $v1, $v1, $t0	# Add ones digit while maintaining tens digit
+	li $t0, 1
+	ble $s0, $t0, findValue
+	
+	# Case where the return value isn't "0X"
+	addi $t1, $t1, -1
+	lbu $v1, 0($t1)		# Tens digit
+	sll $v1, $v1, 8		# Tens digit is 8 bits large, shift it into position
+	lbu $t0, 1($t1)		# Ones digit
+	or $v1, $v1, $t0	# Add ones digit while maintaining tens digit
+	
+	findValue:
 	li $t0, 0		# Loop from end to start of string
 	li $t1, 10		# Constant 10
 	li $t2, 1		# Multiply this however many times by 10 for each digit
--- load_game_test.asm
+++ load_game_test.asm
@@ -18,6 +18,63 @@ la $a0, state
 la $a1, board_filename
 jal load_game
 # You must write your own code here to check the correctness of the function implementation.
+la $t0, state
+
+# Print bot_mancala
+lbu $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print top_mancala
+lbu $a0, 1($t0)
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print bot_pockets
+lbu $a0, 2($t0)
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print top_pockets
+lbu $a0, 3($t0)
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print moves_executed
+lbu $a0, 4($t0)
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print player_turn
+lbu $a0, 5($t0)
+li $v0, 11
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# Print game_board
+addi $a0, $t0, 6
+li $v0, 4
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
 
 li $v0, 10
 syscall
